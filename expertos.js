7a78661412803f8cef2ae09bf325741c0c51f7e8
aguileracisf-cmyk <aguileracisf@gmail.com>
2026-04-30 08:18:52 -0500

    Delete expertos.js

--- expertos.js
+++
@@ -1,8 +0,0 @@
-import { createBrowserClient } from '@supabase/ssr'
-
-export function createClient() {
-  return createBrowserClient(
-    'https://ktyozwtzsjnrqadbthui.supabase.co',
-    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imt0eW96d3R6c2pucnFhZGJ0aHVpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzc1MDg1NzgsImV4cCI6MjA5MzA4NDU3OH0.8HnAehelgr_uK1iVDaSF8kbBwQ5eyEgGwKwoYHqGMf8'
-  )
-}
